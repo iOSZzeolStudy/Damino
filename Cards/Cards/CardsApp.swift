783f3ba55ecc1f1b1555650f703e35d5771cce0f
daminoworld <ekadls757@gmail.com>
2023-03-10 15:50:00 +0900

    Add: Cards App

---
+++ Cards/Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by 김담인 on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cards/Cards/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Cards
+//
+//  Created by 김담인 on 2023/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            PersonCardView(person: person1)
+                .listRowSeparator(.hidden)
+            PersonCardView(person: person2)
+                .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
