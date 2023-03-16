3d3cc4223843a5a9669aff7f7129574268f72193
daminoworld <ekadls757@gmail.com>
2023-03-16 11:11:15 +0900

    Add: NoteSwiftUI APP

---
+++ NoteSwiftUI/NoteSwiftUI/NoteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteSwiftUIApp.swift
+//  NoteSwiftUI
+//
+//  Created by 김담인 on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct NoteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
