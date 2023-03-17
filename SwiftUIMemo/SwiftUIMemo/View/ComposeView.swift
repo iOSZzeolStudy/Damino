0a8075ae1fb21d7cdfe23d3977d44790d627dfc2
daminoworld <ekadls757@gmail.com>
2023-03-17 10:19:46 +0900

    Feat: Edit Memo Feature

--- SwiftUIMemo/SwiftUIMemo/View/ComposeView.swift
+++ SwiftUIMemo/SwiftUIMemo/View/ComposeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ComposeView: View {
     @EnvironmentObject var store: MemoStore
+    
+    var memo: Memo? = nil
+    
     @Environment(\.dismiss) var dismiss
+    
     @State private var content: String = ""
     
     var body: some View {
@@ -19,16 +23,33 @@ struct ComposeView: View {
                 // 둘다 서로 바인딩 되어있어 two-way 바인딩
                 TextEditor(text: $content)
                     .padding()
+                    .onAppear() {
+                        if let memo = memo {
+                            content = memo.content
+                        }
+                    }
             }
-            .navigationTitle("새 메모")
+            .navigationTitle(memo != nil ? "메모 편집" : "새 메모")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItemGroup(placement: .navigationBarLeading)
-                {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
                     Button {
                         dismiss()
                     } label: {
                         Text("취소")
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        if let memo = memo {
+                            store.update(memo: memo, content: content)
+                        } else {
+                            store.insert(memo: content)
+                        }
+                        dismiss()
+                    } label: {
+                        Text("저장")
                     }
                 }
             }
@@ -42,3 +63,4 @@ struct ComposeView_Previews: PreviewProvider {
             .environmentObject(MemoStore())
     }
 }
+ 
--- SwiftUIMemo/SwiftUIMemo/View/DetailView.swift
+++ SwiftUIMemo/SwiftUIMemo/View/DetailView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct DetailView: View {
     //Memo에 Published로 설정한 속성이 바뀌면 뷰도 바뀜
     @ObservedObject var memo: Memo
+    
     @EnvironmentObject var store: MemoStore
+    
+    @State private var showCompoesr: Bool = false
     
     var body: some View {
         VStack {
@@ -30,12 +33,26 @@ struct DetailView: View {
         }
         .navigationTitle("메모 보기")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showCompoesr = true
+                } label: {
+                    Image(systemName:  "square.and.pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showCompoesr) {
+            ComposeView(memo: memo)
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(memo: Memo(content: "Hello"))
-            .environmentObject(MemoStore())
+        NavigationView {
+            DetailView(memo: Memo(content: "Hello"))
+                .environmentObject(MemoStore())
+        }
     }
 }
